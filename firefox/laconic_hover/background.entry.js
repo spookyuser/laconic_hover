a679634a2f9dd11a011187857b88682a9b159c92
spookyuser <spookyUnknownUser@users.noreply.github.com>
2017-01-04 14:49:02 +0200

    Refactor

--- firefox/laconic_hover/background.entry.js
+++
@@ -1,7 +0,0 @@
-import attachToLinks from '../Laconic_Hover.user.js'; // jshint ignore:line
-(function() {
-    'use strict';
-    console.log("Launching Addon Script");
-    attachToLinks();
-
-})();
