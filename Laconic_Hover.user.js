229ca89bc57bea454d4acc559957173e94bb0546
spookyuser <spookyUnknownUser@users.noreply.github.com>
2017-01-09 22:45:35 +0200

    Userscript settings

--- Laconic_Hover.user.js
+++ Laconic_Hover.user.js
@@ -1,16 +1,15 @@
 // ==UserScript==
 // @name         Laconic Hover
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        http://tvtropes.org/*
+// @version      0.2
+// @description  Displays laconic text when hovering over a tvtropes link.
+// @author       spookyUser
+// @match        *://tvtropes.org/*
 // @grant        none
 // @require      https://code.jquery.com/jquery.min.js
-// @require      https://raw.githubusercontent.com/briancherne/jquery-hoverIntent/master/jquery.hoverIntent.js
+// @require      https://cdn.jsdelivr.net/qtip2/3.0.3/jquery.qtip.min.js
+// @resource     https://cdn.jsdelivr.net/qtip2/3.0.3/jquery.qtip.min.css
 // ==/UserScript==
-// Working with grabLaconicText on dynamic hover function.
-/*jshint esversion: 6 */
 
 $(document).ready(function () {
     // Start
@@ -31,9 +30,9 @@ function setQtipContent(jQueryElement, currentUrl) {
         overwrite: false,
         position: {
             viewport: $(window),
-            my: 'center right',  // Position my top left...
-            at: 'center left', // at the bottom right of...
-            target: $(jQueryElement), // my target
+            my: 'center right',
+            at: 'center left',
+            target: $(jQueryElement),
             adjust: {
                 x: -10
             },
@@ -87,7 +86,6 @@ function convertCamelTitle(currentUrl) {
 function parseLaconic(response) {
     var parsedLaconicContent;
     // Find the main page element that contains the laconic text and remove whitespace
-    // https://stackoverflow.com/questions/3422949/jquery-remove-all-child-elements-and-leave-text
     parsedLaconicContent = $(response).find(".indent").remove().end();
     parsedLaconicContent = $(parsedLaconicContent).find(".page-content").first().text().trim();
     if (response.indexOf("Inexact title") >= 0) {
