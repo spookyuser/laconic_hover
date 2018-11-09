be2000ba1a8a0d379690c7869887d47263215367
spookyuser <spookyUnknownUser@users.noreply.github.com>
2018-11-09 07:04:40 +0200

    Add basic tippy.js module

--- src/laconic-hover.js
+++ src/laconic-hover.js
@@ -1,5 +1,34 @@
-function start() {
-    console.log("Build tes")
-}
+import tippy from "tippy.js";
+import "tippy.js/dist/tippy.css";
 
-start();
+const HOVER_SELECTOR = ".twikilink";
+const INITIAL_CONTENT = "Loading...";
+
+const state = {
+  isFetching: false,
+  canFetch: true
+};
+
+console.log("Starting script");
+
+tippy(HOVER_SELECTOR, {
+  content: INITIAL_CONTENT,
+  async onShow(tip) {
+    if (state.isFetching || !state.canFetch) return;
+
+    state.isFetching = true;
+    state.canFetch = false;
+
+    try {
+      const url = tip.reference.href
+    } catch (e) {
+      tip.setContent(`Fetch failed. ${e}`);
+    } finally {
+      state.isFetching = false;
+    }
+  },
+  onHidden(tip) {
+    state.canFetch = true;
+    tip.setContent(INITIAL_CONTENT);
+  }
+});
