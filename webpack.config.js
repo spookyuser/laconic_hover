974b6db839b396b5aeeceffe73efed201b3a0234
spookyuser <spookyUnknownUser@users.noreply.github.com>
2018-11-10 00:13:16 +0200

    Add optimization
    From https://github.com/sindresorhus/refined-github/blob/65fd58a1f1505ff348e3a9111ccda1236c3b563f/webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   devtool: "sourcemap",
@@ -34,6 +35,24 @@ module.exports = {
       }
     ]),
     new CleanWebpackPlugin(["distribution"])
-  ]
-  // TODO: Add optimization
+  ],
+  optimization: {
+    // From https://github.com/sindresorhus/refined-github/blob/65fd58a1f1505ff348e3a9111ccda1236c3b563f/webpack.config.js
+    // Without this, function names will be garbled and enableFeature won't work
+    concatenateModules: true,
+
+    // Automatically enabled on prod; keeps it somewhat readable for AMO reviewers
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          mangle: false,
+          compress: false,
+          output: {
+            beautify: true,
+            indent_level: 2 // eslint-disable-line camelcase
+          }
+        }
+      })
+    ]
+  }
 };
