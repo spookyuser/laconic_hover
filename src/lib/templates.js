bd69d7ab562d9818c40959da9ac1603061cb4cb4
spookyuser <spookyUnknownUser@users.noreply.github.com>
2019-01-14 11:49:28 +0200

    Document hoverTemplate

--- src/lib/templates.js
+++ src/lib/templates.js
@@ -1,6 +1,17 @@
 import html from "innerself";
 import { darkModeEnabled } from "./utils";
 
+/** A small function that generates the custom HTML tippy will show when hovering on a trope
+ * 
+ * The HTML is generated with the package innerself
+ * It changes the title's color depending on whether dark mode 
+ * is enabled. The trope's title and laconic description 
+ * are interpolated into the html. 
+ * 
+ * @see https://github.com/stasm/innerself
+ * @returns {String} HTML describing a "card" with the trope's title and laconic  
+ * @param {Trope} trope An instance of a Trope object 
+ */
 export function hoverTemplate(trope) {
   return html`
     <div
