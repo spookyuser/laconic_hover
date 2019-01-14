0acf70f8b5e586157fb43b9ddf2f89ce45e3cb65
spookyUnknownUser <spookyUnknownUser@users.noreply.github.com>
2019-01-14 11:56:39 +0200

    Refactor hoverTemplate() (#7)
    * Merge html templates
    
    * Document hoverTemplate
    
    * Fix linting errors

--- src/lib/templates.js
+++ src/lib/templates.js
@@ -1,29 +1,31 @@
 import html from "innerself";
 import { darkModeEnabled } from "./utils";
 
+/** A small function that generates the custom HTML tippy will show when hovering on a trope
+ *
+ * The HTML is generated with the package innerself
+ * It changes the title's color depending on whether dark mode
+ * is enabled. The trope's title and laconic description
+ * are interpolated into the html.
+ *
+ * @see https://github.com/stasm/innerself
+ * @returns {String} HTML describing a "card" with the trope's title and laconic
+ * @param {Trope} trope An instance of a Trope object
+ */
 export function hoverTemplate(trope) {
-  // TODO: Refactor out styles
-  const dark = html`
+  return html`
     <div
       style="max-width:300px; text-align: left; font-family: 'Nunito', 'Roboto', 'Helvetica Neue', Helvetica, Arial, sans-serif;"
     >
-      <p style="font-weight:bold; color: #71e1bc; font-size: 1.2rem">
+      <p
+        style="font-weight:bold; color: ${
+          darkModeEnabled() ? "#71e1bc" : "#0849ab"
+        }; font-size: 1.2rem"
+      >
         ${trope.title}
       </p>
       <hr style="opacity: 0.1; " />
       <p style="font-size: 1rem;">${trope.laconic}</p>
     </div>
   `;
-  const light = html`
-    <div
-      style="max-width:300px; text-align: left; font-family: 'Nunito', 'Roboto', 'Helvetica Neue', Helvetica, Arial, sans-serif;"
-    >
-      <p style="font-weight:bold; color: #0849ab; font-size: 1.2rem">
-        ${trope.title}
-      </p>
-      <hr style="opacity: 0.1; " />
-      <p style="font-size: 1rem;">${trope.laconic}</p>
-    </div>
-  `;
-  return darkModeEnabled() ? dark : light;
 }
