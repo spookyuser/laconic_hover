7b880ebfc04c0b7e772d53cd006db98afb1ff0dc
spookyuser <spookyUnknownUser@users.noreply.github.com>
2019-01-14 11:35:07 +0200

    Merge html templates

--- src/lib/templates.js
+++ src/lib/templates.js
@@ -2,28 +2,19 @@ import html from "innerself";
 import { darkModeEnabled } from "./utils";
 
 export function hoverTemplate(trope) {
-  // TODO: Refactor out styles
-  const dark = html`
+  return html`
     <div
       style="max-width:300px; text-align: left; font-family: 'Nunito', 'Roboto', 'Helvetica Neue', Helvetica, Arial, sans-serif;"
     >
-      <p style="font-weight:bold; color: #71e1bc; font-size: 1.2rem">
+      <p
+        style="font-weight:bold; color: ${
+          darkModeEnabled() ? "#71e1bc" : "#0849ab"
+        }; font-size: 1.2rem"
+      >
         ${trope.title}
       </p>
       <hr style="opacity: 0.1; " />
       <p style="font-size: 1rem;">${trope.laconic}</p>
     </div>
   `;
-  const light = html`
-    <div
-      style="max-width:300px; text-align: left; font-family: 'Nunito', 'Roboto', 'Helvetica Neue', Helvetica, Arial, sans-serif;"
-    >
-      <p style="font-weight:bold; color: #0849ab; font-size: 1.2rem">
-        ${trope.title}
-      </p>
-      <hr style="opacity: 0.1; " />
-      <p style="font-size: 1rem;">${trope.laconic}</p>
-    </div>
-  `;
-  return darkModeEnabled() ? dark : light;
 }
