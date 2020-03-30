40f9aca64d2a4a748ac810aed94be252a7a571a8
spookyuser <spookyuser@users.noreply.github.com>
2020-03-30 18:37:25 +0200

    Drop innerself dependency
    Fixes #10

--- source/templates/hover-template.js
+++ source/templates/hover-template.js
@@ -1,14 +1,12 @@
-import html from "innerself";
+import { html } from "common-tags";
 import { darkModeEnabled } from "../lib/utils";
 
 /** A small function that generates the custom HTML tippy will show when hovering on a trope
  *
- * The HTML is generated with the package innerself
  * It changes the title's color depending on whether dark mode
  * is enabled. The trope's title and laconic description
  * are interpolated into the html.
  *
- * @see https://github.com/stasm/innerself
  * @returns {String} HTML describing a "card" with the trope's title and laconic
  * @param {Trope} trope An instance of a Trope object
  */
@@ -18,9 +16,9 @@ export function hoverTemplate(trope) {
       style="max-width:300px; text-align: left; font-family: 'Nunito', 'Roboto', 'Helvetica Neue', Helvetica, Arial, sans-serif;"
     >
       <p
-        style="font-weight:bold; color: ${
-          darkModeEnabled() ? "#71e1bc" : "#0849ab"
-        }; font-size: 1.2rem"
+        style="font-weight:bold; color: ${darkModeEnabled()
+          ? "#71e1bc"
+          : "#0849ab"}; font-size: 1.2rem"
       >
         ${trope.title}
       </p>
