ff73d371f9becff7eedb8df077b375504475e555
spookyuser <spookyuser@users.noreply.github.com>
2023-01-28 15:52:22 +0200

    Add local storage caching

--- source/lib/Trope.ts
+++ source/lib/Trope.ts
@@ -11,23 +11,38 @@ export class Trope {
   }
 
   async fetchLaconic(): Promise<Trope> {
+    // Check if the trope is already in local storage
+    const stored = localStorage.getItem(this.url);
+    if (stored) {
+      const parsed = JSON.parse(stored);
+      this.title = parsed.title;
+      this.laconic = parsed.laconic;
+      return this;
+    }
+
     let document = await fetchPage(this.getLaconicUrl());
 
     const titleElement = document.querySelector(".entry-title");
     if (titleElement?.textContent) {
-      this.title = titleElement.textContent;
+      this.title = titleElement.textContent.trim();
     } else {
       throw new Error("No laconic title found");
     }
 
     const laconicElement = document.querySelector("#main-article > p");
     if (laconicElement?.textContent) {
-      this.laconic = laconicElement.textContent;
+      this.laconic = laconicElement.textContent.trim();
     } else {
       throw new Error("No laconic description found");
     }
+    this.save();
 
     return this;
+  }
+
+  // Store the object as a JSON string in local storage
+  save(): void {
+    localStorage.setItem(this.url, JSON.stringify(this));
   }
 
   // Regex to replace normal link with link directly to laconic page
