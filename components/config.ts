6e0666956de6aa68897b409e451032ba8fca82ce
spookyuser <16196262+spookyuser@users.noreply.github.com>
2025-03-24 10:57:32 +0200

    Add firefox

--- components/config.ts
+++ components/config.ts
@@ -1,11 +1,6 @@
 export const HOVER_SELECTOR = ".twikilink",
   DARK_MODE_COOKIE = "night-vision=true",
-  // Strings
   INITIAL_CONTENT = "Loading…",
-  NO_LACONIC = "No laconic page =(",
-  NO_LACONIC_ERROR = "Inexact title",
-  // Misc
   DARK_THEME = "dark",
   LIGHT_THEME = "light",
-  CACHE_NAME = "laconic-hover-cache",
   DEFAULT_CHARACTER_ENCODING = "iso-8859-1";
